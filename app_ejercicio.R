a004dd5ba21341fcc502706a7d9a9a51ceb67ffd
Pablo Tiscornia <pablotisco@gmail.com>
2023-02-20 19:43:28 -0600

    agrego dstools al flujo

--- app_ejercicio.R
+++ app_ejercicio.R
@@ -12,6 +12,7 @@ library(DT)
 library(leaflet)
 library(makeup)
 
+
 #.............................UI.............................
 
 ### Defino los paneles unicamente en la UI.
@@ -112,26 +113,53 @@ server <- function(input, output, session) {
                   env = environment())
   
   
-  
+  # Lista que suplanta al parmesan_input() 
+  list_inputs <- reactive({
+    list(
+      "Departamento" = input$departamentoId,
+      "Género" = input$generoId
+    )
+    
+  })
   
   ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ##                                  PASO III                                ----
   ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   ### Base reactiva en función de filtros
-  data_filter <- reactive({
-    req(data_load())
-    df <- data_load()
+  # data_filter <- reactive({
+  #   req(data_load())
+  #   df <- data_load()
+  #   
+  #   depto <- input$departamentoId
+  #   genero_sel <- input$generoId
+  #   
+  #   #print(genero_sel)
+  #   if (!is.null(depto)) df <- df |> filter(departamento %in% depto)
+  #   if (!is.null(genero_sel)) df <- df |> filter(genero %in% genero_sel)
+  #   df
+  # })
+  
+  #### FILTRADO CON dsapptools
+  dic_load <- reactive({
     
-    depto <- input$departamentoId
-    genero_sel <- input$generoId
-    
-    #print(genero_sel)
-    if (!is.null(depto)) df <- df |> filter(departamento %in% depto)
-    if (!is.null(genero_sel)) df <- df |> filter(genero %in% genero_sel)
-    df
+    dic <- homodatum::create_dic(data.table::fread("data/lideres.csv"))
+    dic
   })
   
+  
+  data_down <- reactive({
+    req(parmesan_input())
+    ls <- parmesan_input()
+    
+    df <- dsapptools::data_filter(data = data_load(), 
+                                  dic = dic_load(), 
+                                  var_inputs = ls,
+                                  special_placeholder = NULL,
+                                  .id = "id")
+    
+    df
+  })
   
   # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # ##                                  PASO IV                                 ----
@@ -139,24 +167,57 @@ server <- function(input, output, session) {
   
   ### Etapa de filtrado
   
+  # data_viz <- reactive({
+  #   
+  #   req(data_down())
+  #   req(input$viz_selection) 
+  #   
+  #   df <- data_down()
+  #   
+  #   if(input$viz_selection == "map") {
+  #     df <- df |> select(departamento)
+  #   }
+  #   
+  #   if(input$viz_selection == "bar") {
+  #     df <- df |> select(genero)
+  #   }
+  #   
+  #   if(input$viz_selection == "line") {
+  #     df <- df |> select(periodo)
+  #   }
+  #   
+  #   df
+  #   
+  # })
+  
+  var_to_viz <- reactive({
+    req(input$viz_selection)
+    
+    var <- NULL
+    
+    if(input$viz_selection %in% c("map")) var <- "Departamento"
+    
+    if(input$viz_selection %in% c("bar")) var <- "Género"
+    
+    if(input$viz_selection %in% c("line")) var <- "periodo"
+    
+    var
+  })
+  
   data_viz <- reactive({
     
-    req(data_filter())
-    req(input$viz_selection) 
+    if(input$viz_selection == "table") return()
     
-    df <- data_filter()
+    req(data_down())
+    req(var_to_viz())
     
-    if(input$viz_selection == "map") {
-      df <- df |> select(departamento)
-    }
+    if(nrow(data_viz()) == 0)return()
     
-    if(input$viz_selection == "bar") {
-      df <- df |> select(genero)
-    }
+    df <- as_tibble(data_down())
     
-    if(input$viz_selection == "line") {
-      df <- df |> select(periodo)
-    }
+    df <- df |> 
+      dsapptools::variable_selection(viz = input$viz_selection, 
+                                     path = NULL, var_to_viz())
     
     df
     
@@ -202,55 +263,55 @@ server <- function(input, output, session) {
   # 
   # ### Renderizo los gráficos
   
-  output$lflt_viz <- leaflet::renderLeaflet({
-    if(input$viz_selection != "map") return()
-    viz_save()
-  })
-  
-  output$bar_viz <- renderPlot({
-    if(input$viz_selection != "bar") return()
-    viz_save()
-  })
-  
-  output$table_viz <- DT::renderDT({
-    if(input$viz_selection != "table") return()
-    viz_save()
-  })
-  
-  output$line_viz <- renderPlot({
-    if(input$viz_selection != "line") return()
-    viz_save()
-  })
-  
-  # output$test <- renderPrint({
-  #   data_viz()
+  # output$lflt_viz <- leaflet::renderLeaflet({
+  #   if(input$viz_selection != "map") return()
+  #   viz_save()
+  # })
+  # 
+  # output$bar_viz <- renderPlot({
+  #   if(input$viz_selection != "bar") return()
+  #   viz_save()
+  # })
+  # 
+  # output$table_viz <- DT::renderDT({
+  #   if(input$viz_selection != "table") return()
+  #   viz_save()
+  # })
+  # 
+  # output$line_viz <- renderPlot({
+  #   if(input$viz_selection != "line") return()
+  #   viz_save()
   # })
   
-  
-  ## Genero Output a partir del render creado anteriormente
-  output$viz <- renderUI({
-    
-    if(is.null(input$viz_selection)) return()
-    
-    if(input$viz_selection == "map"){
-      leaflet::leafletOutput("lflt_viz")
-      
-    } else if(input$viz_selection == "bar"){
-      plotOutput("bar_viz")
-      
-    } else if(input$viz_selection == "table"){
-      DT::DTOutput("table_viz")
-      
-    } else if(input$viz_selection == "line"){
-      plotOutput("line_viz")
-    }
-    
+  output$test <- renderPrint({
+    data_viz()
   })
   
   
+  # Genero Output a partir del render creado anteriormente
   # output$viz <- renderUI({
-  #   verbatimTextOutput("test")
+  # 
+  #   if(is.null(input$viz_selection)) return()
+  # 
+  #   if(input$viz_selection == "map"){
+  #     leaflet::leafletOutput("lflt_viz")
+  # 
+  #   } else if(input$viz_selection == "bar"){
+  #     plotOutput("bar_viz")
+  # 
+  #   } else if(input$viz_selection == "table"){
+  #     DT::DTOutput("table_viz")
+  # 
+  #   } else if(input$viz_selection == "line"){
+  #     plotOutput("line_viz")
+  #   }
+  # 
   # })
+  # 
+  
+  output$viz <- renderUI({
+    verbatimTextOutput("test")
+  })
   
 }
 
